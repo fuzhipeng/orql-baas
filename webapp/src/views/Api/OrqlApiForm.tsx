cae9964a2e5d73d7d6a28d3d7c676488e2bef0ab
qiushijie <qiushijie@outlook.com>
2019-04-20 18:04:38 +0800

    分离orql编辑组件

--- webapp/src/views/Api/OrqlApiForm.tsx
+++ webapp/src/views/Api/OrqlApiForm.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import {FormComponentProps} from 'antd/lib/form';
 import {FormItemLayout, OrqlOps} from '../../config';
 import {Api, Association, Schema} from '../../beans';
-import OrqlTree, {ExpMap, getExpAndOrder, OrderMap} from './OrqlTree';
-import Parser from 'orql-parser';
-import {OrqlNode, OrqlExp, OrqlItem, OrqlLogicExp, OrqlLogicOp, OrqlNestExp, OrqlCompareExp, OrqlParam, OrqlValue, OrqlColumn, OrqlNull, OrqlOrder} from 'orql-parser/lib/OrqlNode';
+import OrqlTree from './OrqlTree';
 
 const { TextArea } = Input;
 
@@ -17,325 +15,29 @@ export interface ApiFormProps extends FormComponentProps {
 }
 
 interface IState {
-  op: string;
   visual: boolean;
-  array: boolean;
-  schemaName?: string;
-  selectedKeys: string[];
-  expMap: ExpMap;
-  orderMap: OrderMap;
-  expandedKeys: string[];
-}
-
-interface KeyTmp {
-  key: string;
-  arr: string[];
-}
-
-interface SelectItem {
-  name: string;
-  array: boolean;
-  exp?: string;
-  order?: string;
-  columns: string[];
-  children: SelectItem[];
-  selectAll: boolean;
-}
-
-// orql表达式转string
-function orqlExpToString(orqlExp: OrqlExp): string {
-  if (orqlExp instanceof OrqlNestExp) {
-    return '(' + orqlExpToString(orqlExp) + ')';
-  }
-  if (orqlExp instanceof OrqlLogicExp) {
-    const left = orqlExpToString(orqlExp.left);
-    const op = orqlExp.op == OrqlLogicOp.And ? '&&' : '||';
-    const right = orqlExpToString(orqlExp.right);
-    return `${left} ${op} ${right}`;
-  }
-  if (orqlExp instanceof OrqlCompareExp) {
-    let exp = orqlExp.left.name;
-    exp += ` ${orqlExp.op} `;
-    if (orqlExp.right instanceof OrqlParam) {
-      exp += '$' + orqlExp.right.name;
-    } else if (orqlExp.right instanceof OrqlValue) {
-      if (orqlExp.right instanceof OrqlNull) {
-        exp += 'null';
-      } else {
-        exp += orqlExp.right.toString();
-      }
-    } else if (orqlExp.right instanceof OrqlColumn) {
-      exp += orqlExp.right.name;
-    }
-    return exp;
-  }
-  throw new Error('');
-}
-
-// orql orders转string
-function orqlOrdersToString(orders: OrqlOrder[]): string {
-  return 'order ' + orders.map(order => `${order.columns.map(column => column.name).join(' ')} ${order.sort}`).join(', ');
-}
-
-// select item转orql
-function selectItemToOrql(selectItem: SelectItem): string {
-  let orql = selectItem.name;
-  orql += getExpAndOrder(selectItem.exp, selectItem.order);
-  // 全放一个数组先
-  // const childOrqlArr = [
-  //   ...selectItem.columns,
-  //   ...selectItem.children
-  //     .map(child => selectItemToOrql(child))
-  // ];
-  const childOrqlArr: string[] = [];
-  if (selectItem.selectAll) {
-    childOrqlArr.push('*');
-    childOrqlArr.push(...selectItem.columns.map(column => '!' + column));
-  } else {
-    childOrqlArr.push(...selectItem.columns);
-  }
-  childOrqlArr.push(...selectItem.children.map(child => selectItemToOrql(child)));
-  if (childOrqlArr.length > 0) {
-    const start = selectItem.array ? ': [' : ': {';
-    const end = selectItem.array ? ']' : '}';
-    orql += start + childOrqlArr.join(', ') + end;
-  }
-  return orql;
-}
-
-interface OrqlItemWrapper {
-  path: string;
-  item: OrqlItem;
-  schema: Schema;
-  isRoot?: boolean;
-}
-
-function isArray(association: Association) {
-  return association.type == 'hasMany' || association.type == 'belongsToMany';
+  orql?: string;
 }
 
 export default Form.create()(class OrqlApiForm extends React.Component<ApiFormProps, IState> {
 
-  private getSchema = (name: string) => {
-    return this.props.schemas.find(schema => schema.name == name)!
-  }
-
-  private getOrqlParseObject = (orql?: string): {selectedKeys: string[], expMap: ExpMap, orderMap: OrderMap, tree?: OrqlNode} => {
-    const selectedKeys: string[] = [];
-    const expMap: ExpMap = {};
-    const orderMap: OrderMap = {};
-    if (!orql) return {selectedKeys, expMap, orderMap};
-    const orqlTree = Parser.parse(orql);
-    const root = orqlTree.item;
-    // 获取root
-    selectedKeys.push(root.name);
-    const stack: OrqlItemWrapper[] = [{
-      item: root,
-      path: root.name,
-      schema: this.getSchema(root.name)
-    }];
-    while (stack.length > 0) {
-      const {item, schema, path} = stack.pop()!;
-      if (item.where && item.where.exp) {
-        // 获取exp
-        expMap[path] = orqlExpToString(item.where.exp);
-      }
-      if (item.where && item.where.orders && item.where.orders.length > 0) {
-        orderMap[path] = orqlOrdersToString(item.where.orders);
-      }
-      if (item.children) {
-        for (const childItem of item.children) {
-          const column = schema.columns.find(column => column.name == childItem.name);
-          const childPath = `${path}.${childItem.name}`;
-          if (column) {
-            // 获取column
-            selectedKeys.push(`column.${childPath}`);
-            continue;
-          }
-          const association = schema.associations.find(association => association.name == childItem.name);
-          if (association) {
-            // 获取ref
-            selectedKeys.push(`${isArray(association) ? 'array' : 'object'}.${childPath}`);
-            stack.push({
-              item: childItem,
-              path: childPath,
-              schema: this.getSchema(association.refName)
-            });
-          }
-        }
-      }
-    }
-    return {selectedKeys, expMap, orderMap, tree: orqlTree};
-  }
-
   private orql = this.props.api ? this.props.api.orql : undefined;
 
-  private parseObject = this.getOrqlParseObject(this.orql);
-
-  private root = this.parseObject.tree ? this.parseObject.tree.item : undefined;
-
   state: IState = {
-    op: 'query',
     visual: !!this.orql,
-    array: this.root ? this.root.isArray : false,
-    selectedKeys: this.parseObject.selectedKeys,
-    expMap: this.parseObject.expMap,
-    orderMap: this.parseObject.orderMap,
-    schemaName: this.root ? this.root.name : undefined,
-    expandedKeys: []
+    orql: this.orql
   }
 
-  private handleChangeSelectKeys = (selectedKeys: string[]) => {
-    this.setState({
-      selectedKeys
-    }, this.genOrql);
-  }
-  private handleChangeExpMap = (expMap: ExpMap) => {
-    this.setState({
-      expMap
-    }, this.genOrql);
-  }
-  private getParent = (root: SelectItem, arr: string[]): SelectItem | undefined => {
-    let tmp = root;
-    let i = 2;
-    // console.log('arr', arr);
-    while (i < arr.length - 1) {
-      let key = arr[i];
-      // console.log('i', i, 'key', key);
-      i ++;
-      // console.log('parent', JSON.stringify(tmp));
-      if (!tmp) return undefined;
-      tmp = tmp.children.find(child => child.name == key)!;
-      // console.log('tmp', JSON.stringify(tmp));
-    }
-    return tmp;
-  }
-  private genOrql = () => {
+  handleOrqlChange = (orql: string) => {
     const {form: {setFieldsValue}} = this.props;
-    const {op, selectedKeys, expandedKeys, expMap, orderMap, schemaName, array} = this.state;
-    if (selectedKeys.length == 0) return;
-    console.log(expandedKeys);
-    const selectedKeyTmps: KeyTmp[] = [];
-    const expandedKeyTmps: KeyTmp[] = [];
-    // 先切割缓存起来
-    selectedKeys.forEach(key => {
-      const arr = key.split('.');
-      selectedKeyTmps.push({key, arr});
-    });
-    expandedKeys.forEach(key => {
-      const arr = key.split('.');
-      expandedKeyTmps.push({key, arr});
-    });
-    // 排序，避免父节点不存在
-    expandedKeyTmps.sort((a, b) => a.arr.length - b.arr.length);
-    // 排序，用于记录全选
-    // selectedKeyTmps.sort((a, b) => a.arr.length - b.arr.length);
-    // // 排序，避免父节点不存在
-    // selectedKeyTmps.sort((a, b) => a.arr.length - b.arr.length);
-    // // FIXME root用于全选
-    // if (selectedKeyTmps[0].key != schemaName) {
-    //   selectedKeyTmps.unshift({key: schemaName!, arr: [schemaName!]})
-    // }
-    // 重新拼装成树
-    const root: SelectItem = {
-      name: schemaName!,
-      array,
-      columns: [],
-      children: [],
-      exp: expMap[schemaName!],
-      order: orderMap[schemaName!],
-      selectAll: selectedKeys.indexOf(schemaName!) >= 0
-    };
-    for (const keyTmp of expandedKeyTmps) {
-      // 获取父节点
-      const parent = this.getParent(root, keyTmp.arr)!;
-      // 类型 array object column
-      const type = keyTmp.arr[0];
-      // 名称
-      const name = keyTmp.arr[keyTmp.arr.length - 1];
-      // 根据key路径把keys exp order放回去
-      const selectAll = selectedKeys.indexOf(keyTmp.key) >= 0;
-      if (type == 'array') {
-        parent.children.push({
-          array: true,
-          name,
-          exp: expMap[keyTmp.key],
-          children: [],
-          columns: [],
-          selectAll
-        });
-      } else if (type == 'object') {
-        parent.children.push({
-          array: false,
-          exp: expMap[keyTmp.key],
-          name,
-          children: [],
-          columns: [],
-          selectAll
-        });
-      }
-    }
-    for (let i = 0; i < selectedKeyTmps.length; i ++) {
-      const keyTmp = selectedKeyTmps[i];
-      // 获取父节点
-      const parent = this.getParent(root, keyTmp.arr);
-      if (!parent) {
-        // console.log('root', JSON.stringify(root));
-        // console.log('keyTmps', JSON.stringify(keyTmps));
-        console.warn(`key: ${keyTmp.key} parent not exists`);
-        continue;
-      }
-      // 类型 array object column
-      const type = keyTmp.arr[0];
-      // 名称
-      const name = keyTmp.arr[keyTmp.arr.length - 1];
-      // // 根据key路径把keys exp order放回去
-      // if (type == 'array') {
-      //   parent.children.push({
-      //     array: true,
-      //     name,
-      //     exp: expMap[keyTmp.key],
-      //     children: [],
-      //     columns: []
-      //   });
-      // } else if (type == 'object') {
-      //   parent.children.push({
-      //     array: false,
-      //     exp: expMap[keyTmp.key],
-      //     name,
-      //     children: [],
-      //     columns: []
-      //   });
-      // } else {
-      //   parent.columns.push(name);
-      // }
-      if (type == 'column') {
-        parent.columns.push(name);
-      }
-    }
-    const orql = op + ' ' + selectItemToOrql(root);
     setFieldsValue({orql});
-  }
-  private handleOrqlChange = (orql: string) => {
-    try {
-      const orqlTree = Parser.parse(orql);
-      const parserObject = this.getOrqlParseObject(orql);
-      this.setState({
-        expMap: parserObject.expMap,
-        orderMap: parserObject.orderMap,
-        selectedKeys: parserObject.selectedKeys
-      });
-    } catch (e) {
-      // console.log(e);
-    }
-  }
-  private handleArrayChange = (array: boolean) => {
     this.setState({
-      array
-    }, this.genOrql);
+      orql
+    });
   }
+
   render() {
-    const {op, visual, schemaName, array, selectedKeys, expMap, orderMap} = this.state;
+    const {visual, orql} = this.state;
     const {form: {getFieldDecorator, setFieldsValue}, schemas, groups, currentGroup, api} = this.props;
     return (
       <Form layout="vertical" {...FormItemLayout}>
@@ -388,58 +90,11 @@ export default Form.create()(class OrqlApiForm extends React.Component<ApiFormPr
         </Row>
         {visual && (
           <Row>
-            <Col span={6}>
-              <Form.Item label="操作">
-                {getFieldDecorator('op', {
-                  rules: [{ required: false, message: '请输入操作' }],
-                  initialValue: this.parseObject.tree ? this.parseObject.tree.op : 'query'
-                })(
-                  <Select<string> onSelect={op => this.setState({op})}>
-                    {OrqlOps.map(op => (
-                      <Select.Option key={op} value={op}>{op}</Select.Option>
-                    ))}
-                  </Select>
-                )}
-              </Form.Item>
-            </Col>
-            <Col span={6}>
-              <Form.Item label="实体">
-                {getFieldDecorator('schema', {
-                  rules: [{ required: false }],
-                  initialValue: this.parseObject.tree ? this.parseObject.tree.item.name : undefined
-                })(
-                  <Select<string> onSelect={schemaName => this.setState({schemaName})}>
-                    {schemas.map(schema => (
-                      <Select.Option key={schema.name} value={schema.name}>{schema.name}</Select.Option>
-                    ))}
-                  </Select>
-                )}
-              </Form.Item>
-            </Col>
-            <Col span={6}>
-              <Form.Item label="数组">
-                {getFieldDecorator('array', {
-                  initialValue: array,
-                })(<Switch defaultChecked={array} onChange={this.handleArrayChange} />)}
-              </Form.Item>
-            </Col>
-            <Col span={6}>
-              <Form.Item label="分页">
-                {getFieldDecorator('size', {
-                })(<InputNumber disabled={!array} />)}
-              </Form.Item>
-            </Col>
             <Col span={24}>
               <OrqlTree
-                schemaName={schemaName}
                 schemas={schemas}
-                op={op}
-                expMap={expMap}
-                orderMap={orderMap}
-                onChangeExpMap={this.handleChangeExpMap}
-                selectedKeys={selectedKeys}
-                onChangeSelectKeys={this.handleChangeSelectKeys}
-                onChangeExpandedKeys={expandedKeys => this.setState({expandedKeys})}/>
+                orql={orql}
+                onChange={this.handleOrqlChange}/>
             </Col>
           </Row>
         )}
--- webapp/src/views/Api/OrqlTree.tsx
+++ webapp/src/views/Api/OrqlTree.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import {Popover, Tree} from 'antd';
+import {Col, Form, InputNumber, Popover, Row, Select, Switch, Tree} from 'antd';
 import {Association, Schema} from '../../beans';
 import QueryBuilder from '../../components/QueryBuilder';
+import {OrqlOps} from '../../config';
+import Parser from 'orql-parser';
+import {OrqlNode, OrqlExp, OrqlItem, OrqlLogicExp, OrqlLogicOp, OrqlNestExp, OrqlCompareExp, OrqlParam, OrqlValue, OrqlColumn, OrqlNull, OrqlOrder} from 'orql-parser/lib/OrqlNode';
 
 const {TreeNode} = Tree;
 
@@ -9,16 +12,16 @@ export type ExpMap = {[key: string]: string};
 
 export type OrderMap = {[key: string]: string};
 
-export interface OrqlTreeProps {
-  schemas: Schema[];
-  op: string;
-  schemaName?: string;
-  selectedKeys: string[];
-  onChangeSelectKeys: (keys: string[]) => void;
-  expMap: ExpMap;
-  orderMap: OrderMap;
-  onChangeExpMap: (expMap: ExpMap) => void;
-  onChangeExpandedKeys: (expandedKeys: string[]) => void;
+interface OrqlItemWrapper {
+  path: string;
+  item: OrqlItem;
+  schema: Schema;
+  isRoot?: boolean;
+}
+
+interface KeyTmp {
+  key: string;
+  arr: string[];
 }
 
 function isArray(association: Association) {
@@ -32,6 +35,16 @@ interface TreeNodeTitleProps {
   defaultExp?: string;
   defaultOrder?: string;
   onChange: (exp) => void;
+  selectAll: boolean;
+}
+
+interface SelectItem {
+  name: string;
+  isArray: boolean;
+  exp?: string;
+  order?: string;
+  columns: string[];
+  children: SelectItem[];
   selectAll: boolean;
 }
 
@@ -58,27 +71,280 @@ const TreeNodeTitle = (props: TreeNodeTitleProps) => (
   </Popover>
 )
 
+// orql表达式转string
+function orqlExpToString(orqlExp: OrqlExp): string {
+  if (orqlExp instanceof OrqlNestExp) {
+    return '(' + orqlExpToString(orqlExp) + ')';
+  }
+  if (orqlExp instanceof OrqlLogicExp) {
+    const left = orqlExpToString(orqlExp.left);
+    const op = orqlExp.op == OrqlLogicOp.And ? '&&' : '||';
+    const right = orqlExpToString(orqlExp.right);
+    return `${left} ${op} ${right}`;
+  }
+  if (orqlExp instanceof OrqlCompareExp) {
+    let exp = orqlExp.left.name;
+    exp += ` ${orqlExp.op} `;
+    if (orqlExp.right instanceof OrqlParam) {
+      exp += '$' + orqlExp.right.name;
+    } else if (orqlExp.right instanceof OrqlValue) {
+      if (orqlExp.right instanceof OrqlNull) {
+        exp += 'null';
+      } else {
+        exp += orqlExp.right.toString();
+      }
+    } else if (orqlExp.right instanceof OrqlColumn) {
+      exp += orqlExp.right.name;
+    }
+    return exp;
+  }
+  throw new Error('');
+}
+
+// orql orders转string
+function orqlOrdersToString(orders: OrqlOrder[]): string {
+  return 'order ' + orders.map(order => `${order.columns.map(column => column.name).join(' ')} ${order.sort}`).join(', ');
+}
+
+// select item转orql
+function selectItemToOrql(selectItem: SelectItem): string {
+  let orql = selectItem.name;
+  orql += getExpAndOrder(selectItem.exp, selectItem.order);
+  const childOrqlArr: string[] = [];
+  if (selectItem.selectAll) {
+    childOrqlArr.push('*');
+    childOrqlArr.push(...selectItem.columns.map(column => '!' + column));
+  } else {
+    childOrqlArr.push(...selectItem.columns);
+  }
+  childOrqlArr.push(...selectItem.children.map(child => selectItemToOrql(child)));
+  if (childOrqlArr.length > 0) {
+    const start = selectItem.isArray ? ': [' : ': {';
+    const end = selectItem.isArray ? ']' : '}';
+    orql += start + childOrqlArr.join(', ') + end;
+  }
+  return orql;
+}
+
+export interface OrqlTreeProps {
+  schemas: Schema[];
+  orql?: string;
+  onChange: (orql: string) => void;
+}
+
 interface IState {
+  schemaName?: string;
   expandedKeys: string[];
+  selectedKeys: string[];
+  op: string;
+  isArray: boolean;
+  expMap: ExpMap;
+  orderMap: OrderMap;
 }
 
 class OrqlTree extends React.Component<OrqlTreeProps, IState> {
+
+  private getOrqlParseObject = (orql?: string): {selectedKeys: string[], expMap: ExpMap, orderMap: OrderMap, tree?: OrqlNode} => {
+    const selectedKeys: string[] = [];
+    const expMap: ExpMap = {};
+    const orderMap: OrderMap = {};
+    if (!orql) return {selectedKeys, expMap, orderMap};
+    const orqlTree = Parser.parse(orql);
+    const root = orqlTree.item;
+    // 获取root
+    selectedKeys.push(root.name);
+    const stack: OrqlItemWrapper[] = [{
+      item: root,
+      path: root.name,
+      schema: this.getSchema(root.name)
+    }];
+    while (stack.length > 0) {
+      const {item, schema, path} = stack.pop()!;
+      if (item.where && item.where.exp) {
+        // 获取exp
+        expMap[path] = orqlExpToString(item.where.exp);
+      }
+      if (item.where && item.where.orders && item.where.orders.length > 0) {
+        orderMap[path] = orqlOrdersToString(item.where.orders);
+      }
+      if (item.children) {
+        for (const childItem of item.children) {
+          const column = schema.columns.find(column => column.name == childItem.name);
+          const childPath = `${path}.${childItem.name}`;
+          if (column) {
+            // 获取column
+            selectedKeys.push(`column.${childPath}`);
+            continue;
+          }
+          const association = schema.associations.find(association => association.name == childItem.name);
+          if (association) {
+            // 获取ref
+            selectedKeys.push(`${isArray(association) ? 'array' : 'object'}.${childPath}`);
+            stack.push({
+              item: childItem,
+              path: childPath,
+              schema: this.getSchema(association.refName)
+            });
+          }
+        }
+      }
+    }
+    return {selectedKeys, expMap, orderMap, tree: orqlTree};
+  }
+
+  private getSchema = (name: string) => {
+    return this.props.schemas.find(schema => schema.name == name)!
+  }
+
+  private orql = this.props.orql;
+
+  private parseObject = this.getOrqlParseObject(this.orql);
+
+  private root = this.parseObject.tree ? this.parseObject.tree.item : undefined;
+
   state: IState = {
-    expandedKeys: []
+    schemaName: this.root ? this.root.name : undefined,
+    expandedKeys: [],
+    selectedKeys: this.parseObject.selectedKeys,
+    op: this.parseObject.tree ? this.parseObject.tree.op : 'query',
+    isArray: this.root ? this.root.isArray : false,
+    expMap: this.parseObject.expMap,
+    orderMap: this.parseObject.orderMap
   }
+
   handleCheck = (keys: any) => {
-    const {op, onChangeSelectKeys} = this.props;
-    // console.log(keys.checked);
-    onChangeSelectKeys(keys.checked);
+    this.setState({
+      selectedKeys: keys.checked
+    }, this.genOrql);
   }
+
   handleExpand = (expandedKeys: string[]) => {
-    this.props.onChangeExpandedKeys(expandedKeys);
     this.setState({expandedKeys});
   }
+
+  handleArrayChange = (isArray: boolean) => {
+    this.setState({
+      isArray
+    }, this.genOrql);
+  }
+
+  handleChangeExpMap = (expMap: ExpMap) => {
+    this.setState({
+      expMap
+    }, this.genOrql);
+  }
+
+  private getParent = (root: SelectItem, arr: string[]): SelectItem | undefined => {
+    let tmp = root;
+    let i = 2;
+    // console.log('arr', arr);
+    while (i < arr.length - 1) {
+      let key = arr[i];
+      // console.log('i', i, 'key', key);
+      i ++;
+      // console.log('parent', JSON.stringify(tmp));
+      if (!tmp) return undefined;
+      tmp = tmp.children.find(child => child.name == key)!;
+      // console.log('tmp', JSON.stringify(tmp));
+    }
+    return tmp;
+  }
+
+  private genOrql = () => {
+    const {op, selectedKeys, expandedKeys, expMap, orderMap, schemaName, isArray} = this.state;
+    if (selectedKeys.length == 0) return;
+    console.log(expandedKeys);
+    const selectedKeyTmps: KeyTmp[] = [];
+    const expandedKeyTmps: KeyTmp[] = [];
+    // 先切割缓存起来
+    selectedKeys.forEach(key => {
+      const arr = key.split('.');
+      selectedKeyTmps.push({key, arr});
+    });
+    expandedKeys.forEach(key => {
+      const arr = key.split('.');
+      expandedKeyTmps.push({key, arr});
+    });
+    // 排序，避免父节点不存在
+    expandedKeyTmps.sort((a, b) => a.arr.length - b.arr.length);
+    // 重新拼装成树
+    const root: SelectItem = {
+      name: schemaName!,
+      isArray,
+      columns: [],
+      children: [],
+      exp: expMap[schemaName!],
+      order: orderMap[schemaName!],
+      selectAll: selectedKeys.indexOf(schemaName!) >= 0
+    };
+    for (const keyTmp of expandedKeyTmps) {
+      // 获取父节点
+      const parent = this.getParent(root, keyTmp.arr)!;
+      // 类型 array object column
+      const type = keyTmp.arr[0];
+      // 名称
+      const name = keyTmp.arr[keyTmp.arr.length - 1];
+      // 根据key路径把keys exp order放回去
+      const selectAll = selectedKeys.indexOf(keyTmp.key) >= 0;
+      if (type == 'array') {
+        parent.children.push({
+          isArray: true,
+          name,
+          exp: expMap[keyTmp.key],
+          children: [],
+          columns: [],
+          selectAll
+        });
+      } else if (type == 'object') {
+        parent.children.push({
+          isArray: false,
+          exp: expMap[keyTmp.key],
+          name,
+          children: [],
+          columns: [],
+          selectAll
+        });
+      }
+    }
+    for (let i = 0; i < selectedKeyTmps.length; i ++) {
+      const keyTmp = selectedKeyTmps[i];
+      // 获取父节点
+      const parent = this.getParent(root, keyTmp.arr)!;
+      // 类型 array object column
+      const type = keyTmp.arr[0];
+      // 名称
+      const name = keyTmp.arr[keyTmp.arr.length - 1];
+      if (type == 'column') {
+        parent.columns.push(name);
+      }
+    }
+    const orql = op + ' ' + selectItemToOrql(root);
+    this.props.onChange(orql);
+  }
+
+  componentWillReceiveProps(nextProps: OrqlTreeProps) {
+    if (nextProps.orql != this.orql) {
+      try {
+        const parseObject = this.getOrqlParseObject(nextProps.orql);
+        this.orql = nextProps.orql;
+        const root = parseObject.tree!;
+        this.setState({
+          op: root.op,
+          isArray: root.item.isArray,
+          selectedKeys: parseObject.selectedKeys,
+          expMap: parseObject.expMap,
+          orderMap: parseObject.orderMap
+        });
+      } catch (e) {
+        console.log('parse error');
+      }
+    }
+  }
+
   renderTree() {
-    const {schemas, schemaName, selectedKeys, expMap, orderMap, onChangeExpMap} = this.props;
+    const {schemas} = this.props;
+    const {schemaName, selectedKeys, expandedKeys, expMap, orderMap} = this.state;
     if (!schemaName) return;
-    const {expandedKeys} = this.state;
     const schema = schemas.find(schema => schema.name == schemaName)!;
     const selectAll = selectedKeys.indexOf(schemaName) >= 0;
     return (
@@ -91,7 +357,7 @@ class OrqlTree extends React.Component<OrqlTreeProps, IState> {
         <TreeNode key={schema.name} title={(
           <TreeNodeTitle
             path={schemaName}
-            onChange={exp => onChangeExpMap({...expMap, [schemaName]: exp})}
+            onChange={exp => this.handleChangeExpMap({...expMap, [schemaName]: exp})}
             defaultExp={expMap[schemaName]}
             defaultOrder={orderMap[schemaName]}
             schema={schema}
@@ -124,7 +390,7 @@ class OrqlTree extends React.Component<OrqlTreeProps, IState> {
                       defaultExp={expMap[key]}
                       defaultOrder={orderMap[key]}
                       path={key}
-                      onChange={exp => onChangeExpMap({...expMap, [key]: exp})}
+                      onChange={exp => this.handleChangeExpMap({...expMap, [key]: exp})}
                       title={association.name}
                       schema={schemas.find(schema => schema.name == association.refName)!}
                       selectAll={selectedKeys.indexOf(key) >= 0}/>
@@ -136,8 +402,8 @@ class OrqlTree extends React.Component<OrqlTreeProps, IState> {
     );
   }
   renderRefTree(parentKey: string, array: boolean, refName: string, schemaName: string) {
-    const {schemas, selectedKeys, expMap, orderMap, onChangeExpMap} = this.props;
-    const {expandedKeys} = this.state;
+    const {schemas} = this.props;
+    const {expandedKeys, selectedKeys, expMap, orderMap} = this.state;
     const schema = schemas.find(schema => schema.name == schemaName)!;
     const key = `${array ? 'array' : 'object'}.${parentKey}.${refName}`;
     const selectAll = selectedKeys.indexOf(key) >= 0;
@@ -147,7 +413,7 @@ class OrqlTree extends React.Component<OrqlTreeProps, IState> {
           path={key}
           defaultExp={expMap[key]}
           defaultOrder={orderMap[key]}
-          onChange={exp => onChangeExpMap({...expMap, [key]: exp})}
+          onChange={exp => this.handleChangeExpMap({...expMap, [key]: exp})}
           title={refName}
           schema={schema}
           selectAll={selectAll}/>
@@ -182,10 +448,41 @@ class OrqlTree extends React.Component<OrqlTreeProps, IState> {
     );
   }
   render() {
-    const {schemaName} = this.props;
+    const {schemas} = this.props;
+    const {schemaName, isArray, op} = this.state;
     return (
       <div>
         <p style={{color: 'rgba(0, 0, 0, 0.65)', margin: '5px 0'}}>{schemaName ? '点击名称配置条件' : '请选择实体'}</p>
+        <Row>
+          <Col span={6}>
+            <Form.Item label="操作">
+              <Select<string> value={op} onSelect={op => this.setState({op})}>
+                {OrqlOps.map(op => (
+                  <Select.Option key={op} value={op}>{op}</Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="实体">
+              <Select<string> value={schemaName} onSelect={schemaName => this.setState({schemaName})}>
+                {schemas.map(schema => (
+                  <Select.Option key={schema.name} value={schema.name}>{schema.name}</Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="数组">
+              <Switch defaultChecked={isArray} onChange={this.handleArrayChange} />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="分页">
+              <InputNumber disabled={!isArray} />
+            </Form.Item>
+          </Col>
+        </Row>
         {this.renderTree()}
       </div>
     );
